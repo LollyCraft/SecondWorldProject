b7f9f96e91aea9da209e8a8a6255048778e0a3d1
BuildTools <unconfigured@null.spigotmc.org>
2018-02-24 13:28:51 +0200

    fyuh

---
+++ src/main/java/ro/coderdojo/serverproject/PowerBlocks.java
@@ -0,0 +1,39 @@
+package ro.coderdojo.serverproject;
+
+import java.util.ArrayList;
+import java.util.Random;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+
+
+public class PowerBlocks {
+    
+    public ArrayList<Location> blockLocations = new ArrayList<>();
+  
+    
+    public void placeBlock(World arena){
+            
+            
+            for (double locX = -322; locX >= -368; locX--) {
+		for (double locZ = 48; locZ >= 3; locZ--) {
+			blockLocations.add(new Location(arena, locX, 3.00000, locZ));
+		}
+            }
+
+            
+            Location blockPetLocation = blockLocations.get(new Random().nextInt(blockLocations.size()));
+            Block petBlock = arena.getBlockAt(blockPetLocation);
+            petBlock.setType(Material.EMERALD_BLOCK);
+            System.out.println("*******placed pet block******");
+            
+            Location blockKitLocation = blockLocations.get(new Random().nextInt(blockLocations.size()));
+            Block kitBlock = arena.getBlockAt(blockKitLocation);
+            kitBlock.setType(Material.DIAMOND_BLOCK);
+            System.out.println("*******placed KIT block******");
+            
+            
+        }
+    
+}
